f691fbf3b3db2f9a93c1cab54c25a535581e69c0
Hashem Alkadri <hashema1@learn.cssd.ab.ca>
2022-03-31 14:30:24 -0600

    ye

---
+++ New Unity Project/Assets/scripts/proposal.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//2d game with three lanes, car can go left and right, has to avoid objects on lanes
+//if have extra time, make it show distance with ability to jump, have differennt themes like planes, cars, military etc
+
+//create play space with lanes
+// create main object that goes left and right
+// set up object spwner above lanes above play space
+// set up physics and colliders
+// set up start screen and lse screen
+
+
+
+
+
+
+public class proposal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
