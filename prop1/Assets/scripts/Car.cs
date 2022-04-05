1130401b65339934b7a2a574860b3b32fb5be685
Hashem Alkadri <hashema1@learn.cssd.ab.ca>
2022-04-05 14:13:58 -0600

    bruuh

--- prop1/Assets/scripts/Car.cs
+++ prop1/Assets/scripts/Car.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Car : MonoBehaviour
 {
+    int currentLane;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,11 @@ public class Car : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+         Vector2 transform.position = new Vector2(6, 9);
+        }
     }
+
+    
 }
