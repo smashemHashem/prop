53b28b9893df3f1ceae6a8c1f4b68e3b74042f9c
smashemHashem <101361262+smashemHashem@users.noreply.github.com>
2022-04-06 06:46:09 -0600

    whaaat the road spins!!

--- prop1/Assets/scripts/Car.cs
+++ prop1/Assets/scripts/Car.cs
@@ -4,21 +4,53 @@ using UnityEngine;
 
 public class Car : MonoBehaviour
 {
-    int currentLane;
+    //cache?
+     Rigidbody2D myRigidBody2D;
+    int currentLane = 1;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidBody2D = GetComponent<Rigidbody2D>();
+        // transform.position = new Vector2(0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.UpArrow))
+        //Debug.Log();
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
         {
-         Vector2 transform.position = new Vector2(6, 9);
+         turnLeft();
+        }
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            turnRight();
         }
     }
-
-    
+    private void turnLeft()
+    {
+        if (currentLane == 1)
+        {
+            transform.position = new Vector2 (-2f,transform.position.y);
+            currentLane = 0;
+        }
+        else if(currentLane == 2)
+        {
+            transform.position = new Vector2 (0f,transform.position.y);
+            currentLane = 1;
+        }
+    }
+    private void turnRight()
+    {
+        if (currentLane == 0)
+        {
+            transform.position = new Vector2 (0f,transform.position.y);
+            currentLane=1;
+        }
+       else if(currentLane == 1)
+        {
+            transform.position = new Vector2 (2f,transform.position.y);
+            currentLane = 2;
+        }
+    }
 }
---
+++ prop1/Assets/scripts/Fruit.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    [SerializeField] float min;
+    [SerializeField] float max;
+    //add prefabs for obstacle spawning in inspector adds to list
+[SerializeField] GameObject[] obstaclePrefab;
+    //how often obstacle spawns in seconds 
+[SerializeField]float  SecondSpawn = 0.5f;
+
+//state variable????
+//magic fix for method
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         StartCoroutine(ObstacleSpawn());
+
+    }
+
+    // Update is called once per frame
+
+    
+    
+    IEnumerator ObstacleSpawn()
+    {
+        while(true)
+        // {  int i = Random.Range(1,4);
+        // if(i == 1)
+        // {
+        //     var wanted = -2f;
+        // }
+        // else if (i == 2)
+        // {
+        //     var wanted = 0f;
+        // }
+        // else if (i == 3)
+        // {
+        //     var wanted = 2f;
+        // }
+        {
+            var wanted = Random.Range(min, max);
+            var position = new Vector3(wanted, transform.position.y);
+            GameObject gameObject = Instantiate(obstaclePrefab[Random.Range(0, obstaclePrefab.Length)], position, Quaternion.identity);
+            // yield return new WaitforSeconds(SecondSpawn);
+            Destroy(gameObject, 5f);
+        }
+    }
+    }
+
