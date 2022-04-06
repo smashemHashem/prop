e646d344b3ff57634610ff461890aa7c8d37fcda
Hashem Alkadri <hashema1@learn.cssd.ab.ca>
2022-04-06 14:25:14 -0600

    we ball

---
+++ prop1/Assets/scripts/GameStatus.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class GameStatus : MonoBehaviour
+{
+    [Range(0.1f, 10f)] [SerializeField] float gameSpeed;
+    [SerializeField] TextMeshProUGUI scoreText;
+    float currentScore = 0;
+    float scoreIncrease = 10;
+    // Start is called before the first frame update
+    // void Start()
+    // {
+    //     InvokeRepeating(AddToScore, 1.0, 1.0);
+    // }
+    // void AddToScore(){
+    //     currentScore +=10;
+    // }
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale = gameSpeed;
+    }
+}
